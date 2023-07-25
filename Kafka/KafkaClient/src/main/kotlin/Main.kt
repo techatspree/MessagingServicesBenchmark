8aba45167994c54fd1a1d8e991bc05de6dbfb531
tfink <torsten.fink@akquinet.de>
2023-07-25 13:29:52 +0200

    sent a message

--- Kafka/KafkaClient/src/main/kotlin/Main.kt
+++ Kafka/KafkaClient/src/main/kotlin/Main.kt
@@ -1,22 +1,30 @@
 import org.apache.kafka.clients.admin.Admin
 import org.apache.kafka.clients.admin.AdminClientConfig
 import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.errors.TopicExistsException
+import org.apache.kafka.common.serialization.StringSerializer
 import java.util.*
 import java.util.concurrent.ExecutionException
 
-private const val NO_REPLICATION:Short = 1
+private const val NO_REPLICATION: Short = 1
 
-fun main(args : Array<String>) {
-    if (args.size > 0 ) {
+private const val TOPIC_NAME = "MeasurementTopic"
+
+fun main(args: Array<String>) {
+    if (args.size > 0) {
         System.err.println("CLI arguments are not supported. I will ignore them and continue.")
     }
 
     println("Create topic...")
     val nrOfPartitions = 1
-    val newTopic = NewTopic("MeasurementTopic", nrOfPartitions, NO_REPLICATION)
-    val kafkaConfiguration = Properties()
-    kafkaConfiguration.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092")
+    val newTopic = NewTopic(TOPIC_NAME, nrOfPartitions, NO_REPLICATION)
+
+    val kafkaConfiguration = createKafkaConfiguration()
+
     try {
         with(Admin.create(kafkaConfiguration)) {
             createTopics(listOf(newTopic)).all().get()
@@ -24,4 +32,31 @@ fun main(args : Array<String>) {
     } catch (e: ExecutionException) {
         if (e.cause !is TopicExistsException) throw e
     }
+
+    println("Send a message ....")
+    KafkaProducer<String, String>(kafkaConfiguration).use { producer ->
+        producer.send(
+            ProducerRecord(
+                TOPIC_NAME,
+                "MeasurementKey",
+                "my test message"
+            )
+        ) { recordMetadata: RecordMetadata, ex: Exception? ->
+            when (ex) {
+                null ->
+                    println("Produced message with offset ${recordMetadata.offset()}")
+
+                else ->
+                    ex.printStackTrace()
+            }
+        }
+    }
+}
+
+private fun createKafkaConfiguration(): Properties {
+    val properties = Properties()
+    properties[AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG] = "kafka:9092"
+    properties[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName
+    properties[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.qualifiedName
+    return properties
 }
